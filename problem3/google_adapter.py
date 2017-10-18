7bb616bfbd98621e414082d0307a60865dc15ba0
nayak16 <aatishnayak1a@gmail.com>
2017-10-17 20:00:54 -0700

    Finish problem 3

--- problem3/google_adapter.py
+++ problem3/google_adapter.py
@@ -1,8 +1,55 @@
+from datetime import datetime
 import googlemaps
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class GoogleAdapter():
+    IMPERIAL_UNITS = 'imperial'
+    METRIC_UNITS = 'metric'
+    DRIVING_MODE = 'driving'
+    METERS_IN_MILES = 1609.34
+    SEC_IN_MIN = 60
 
     def __init__(self, api_key):
 
         self.client = googlemaps.Client(key=api_key)
+
+    def _meters_to_miles(self, meters):
+        return round(meters / self.METERS_IN_MILES, 1)
+
+    def _seconds_to_minutes(self, seconds):
+        return seconds / self.SEC_IN_MIN
+
+    def route_estimate(self, origin, destination, departure_time=None):
+
+        departure_time = departure_time if departure_time else datetime.now()
+
+        resp = self.client.distance_matrix(
+            origins=origin,
+            destinations=destination,
+            departure_time=departure_time,
+            mode=self.DRIVING_MODE,
+            units=self.IMPERIAL_UNITS
+        )
+
+        elements = resp['rows'][0]['elements'][0]
+
+        distance = self._meters_to_miles(elements["distance"]["value"])
+        time = self._seconds_to_minutes(elements["duration"]["value"])
+
+        return (distance, time)
+
+
+
+
+
+
+
+
+
+
+
---
+++ problem3/main.py
@@ -0,0 +1,56 @@
+import click
+from datetime import datetime
+import logging
+
+from google_adapter import GoogleAdapter
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("Route Estimate CLI")
+
+API_KEY = "AIzaSyDBAjidO8byrVXcY6bR9yqxbhE4HRp_SaU"
+TIME_FORMAT = "%H:%M"
+
+
+def set_up():
+    return GoogleAdapter(API_KEY)
+
+
+@click.command()
+@click.option('--origin', '-o', multiple=True,
+              help="Origin Address"
+              )
+@click.option('--destination', '-d', multiple=True,
+              help="Destination Address"
+              )
+@click.option('--time', '-t',
+              help="Departure time formatted as HH:MM in 24 hour format "
+                   "i.e. 16:20. If not specified, defaults to now."
+              )
+def main(origin, destination, time):
+
+    origin = list(origin)
+    destination = list(destination)
+    google_adapter = set_up()
+
+    if time:
+        try:
+            time = datetime.strptime(time, TIME_FORMAT)
+        except ValueError:
+            raise click.BadParameter(
+                "Time must be in {} format".format(TIME_FORMAT)
+            )
+
+    logger.info("Calculating time and distance from "
+                "{} to {}.".format(origin, destination))
+
+    (dist, time) = google_adapter.route_estimate(
+        list(origin), list(destination)
+    )
+
+    logger.info(
+        "This route will take {} miles and {} minutes".format(dist, time)
+    )
+
+
+if __name__ == "__main__":
+    main()
